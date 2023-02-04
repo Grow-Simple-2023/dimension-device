78e9ff2cce8756eda22e8636d02dd3c58eff2a1a
Shashank P <shashankp2832@gmail.com>
2023-02-04 23:31:48 +0530

    Added calliberate

--- calliberate.py
+++ calliberate.py
@@ -23,18 +23,26 @@ sensor3 = adafruit_vl53l0x.VL53L0X(i2c, address=0x2d)
 total1, total2, total3 = 62.74, 65.65, 63.53
 window_size = 20
 
-
 while True:
     distance1 = get_average(sensor1, window_size)
-    sleep(0.5)
+    sleep(0.2)
     distance2 = get_average(sensor2, window_size)
-    sleep(0.5)
+    sleep(0.2)
     distance3 = get_average(sensor3, window_size)
-    sleep(0.5)
-    print("---------------------------------")
-    print("Sensor 1:", total1 - distance1/10, "cm")
-    print("Sensor 2:", total2 - distance2/10, "cm")
-    print("Sensor 3:", total3 - distance3/10, "cm")
-    print("---------------------------------")
+    sleep(0.2)
+    print(f'{total1 - distance1/10},{total2 - distance2/10},{total3 - distance3/10}')
+    # print("---------------------------------")
+    # print("Sensor 1:", total1 - distance1/10, "cm")
+    # print("Sensor 2:", total2 - distance2/10, "cm")
+    # print("Sensor 3:", total3 - distance3/10, "cm")
+    # print("---------------------------------")
     break
     # os.system('cls' if os.name == 'nt' else 'clear')
+
+# [0, 5, 10, 15, 20, 25, 30, 57.7, 51.7, 46.7, 41.7, 36.7, 31.7]
+
+# [[-0.024, -0.12, 0.07], [3.985, 2.84, 6.335], [9.155, 7.865, 11.38], [14.545, 14.595, 17.125],
+#     [22.89, 20.945, 22.745], [27.78, 26.685, 27.99], [33.4, 32.47, 33.345]]
+
+# [[56.92, 57.035, 57.39], [51.95, 51.51, 51.915], [47.145, 45.815, 46.895],
+#     [40.26, 40.29, 40.85], [34.44, 34.025, 34.825], [28.715, 28.16, 29.88]]
