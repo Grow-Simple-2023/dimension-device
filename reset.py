20da54cd74704247d1ab9d06989dd0992a88f86c
Shashank P <shashankp2832@gmail.com>
2023-02-04 19:49:03 +0530

    Added print sattements

--- reset.py
+++ reset.py
@@ -9,4 +9,5 @@ GPIO_ports = [22, 23, 24, 27, 25]
 for i in range(no_of_sensors):
     GPIO.setup(GPIO_ports[i], GPIO.OUT)
     GPIO.output(GPIO_ports[i], GPIO.LOW)
+    print(f"Set GPIO {GPIO_ports[i]} to LOW")
     sleep(0.1)
