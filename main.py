2e4b11b6aeb890ecb41c3d00b93c18e3a613e7a0
Shashank P <shashankp2832@gmail.com>
2023-02-04 20:39:01 +0530

    Added exact height code

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ sensor2 = adafruit_vl53l0x.VL53L0X(i2c, address=0x2c)
 sensor3 = adafruit_vl53l0x.VL53L0X(i2c, address=0x2d)
 
 off1, off2, off3 = 4.977, 8.135, 5.637
-
+sensor_height = 57.7
 
 while True:
     range1 = sensor1.range - off1*10
     range2 = sensor2.range - off2*10
     range3 = sensor3.range - off3*10
-    print("Sensor 1:", max(range1/10, 0), "cm")
-    print("Sensor 2:", max(range2/10, 0), "cm")
-    print("Sensor 3:", max(range3/10, 0), "cm")
+    print("Sensor 1:", max(sensor_height - range1/10, 0), "cm")
+    print("Sensor 2:", max(sensor_height - range2/10, 0), "cm")
+    print("Sensor 3:", max(sensor_height - range3/10, 0), "cm")
     time.sleep(1)
