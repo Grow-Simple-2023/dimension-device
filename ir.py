6658dc02ea6d0c60d6efde9d63d70793d34d56ea
Shashank P <shashankp2832@gmail.com>
2023-02-05 15:15:06 +0530

    added seaprate ir.py

---
+++ ir.py
@@ -0,0 +1,17 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+print("Initializing Conveyor ...")
+sensor_pins = [21, 26]
+
+
+for pin in sensor_pins:
+    print("Setting up pin in sensor: ", pin)
+    GPIO.setup(pin, GPIO.IN)
+
+
+while True:
+    out_put_array = [GPIO.input(pin) for pin in sensor_pins]
+    print(out_put_array)
+    sleep(0.1)
