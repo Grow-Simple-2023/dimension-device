387225dc71e9bf36197e8bac2539e2d53bdd15fd
Shashank P <shashankp2832@gmail.com>
2023-02-05 14:29:56 +0530

    changed order

--- Driver.py
+++ Driver.py
@@ -15,6 +15,51 @@ import cv2
 import sys
 import requests
 
+# -----------------------CONVEYOR BELT----------------------------------
+# CONVEYOR BELT
+print("Initializing Conveyor ...")
+sensor_pins = [40, 37]
+
+
+for pin in sensor_pins:
+    print("Setting up pin in sensor: ", pin)
+    GPIO.setup(pin, GPIO.IN)
+
+conveyer_ports = [36, 38]
+for pin in converyer_ports:
+    print("Setting up pin in coveyor: ", pin)
+    GPIO.setup(pin, GPIO.OUT)
+
+sleep(0.1)
+
+
+def in_config() -> bool:
+    out_put_array = [GPIO.input(pin) for pin in sensor_pins]
+    return out_put_array[0] == 1 and out_put_array[1] == 0
+
+
+def centre_config() -> bool:
+    out_put_array = [GPIO.input(pin) for pin in sensor_pins]
+    return out_put_array[1] == 1
+
+
+def out_config() -> bool:
+    out_put_array = [GPIO.input(pin) for pin in sensor_pins]
+    return out_put_array[0] == 0 and out_put_array[1] == 0
+
+
+def start() -> None:
+    GPIO.output(conveyer_ports[1], GPIO.LOW)
+    GPIO.output(conveyer_ports[0], GPIO.HIGH)
+    return
+
+
+def stop() -> None:
+    GPIO.output(conveyer_ports[0], GPIO.LOW)
+    GPIO.output(conveyer_ports[1], GPIO.HIGH)
+    return
+# -------------------------------------------------------------------
+
 # -----------------------TOF SENSORS----------------------------------
 # TOF SENSORS
 print("Initializing TOF sensors ...")
@@ -50,53 +95,6 @@ print("Initializing Send Data ...")
 def send_dimensions():
     pass
 sleep(0.1)
-# -------------------------------------------------------------------
-
-
-# -----------------------CONVEYOR BELT----------------------------------
-# CONVEYOR BELT
-print("Initializing Conveyor ...")
-sensor_pins = [40, 37]
-
-
-for pin in sensor_pins:
-    print("Setting up pin in sensor: ", pin)
-    GPIO.setup(pin, GPIO.IN)
-
-conveyer_ports = [36, 38]
-for pin in converyer_ports:
-    print("Setting up pin in coveyor: ", pin)
-    GPIO.setup(pin, GPIO.OUT)
-
-sleep(0.1)
-
-
-
-def in_config() -> bool:
-    out_put_array = [GPIO.input(pin) for pin in sensor_pins]
-    return out_put_array[0] == 1 and out_put_array[1] == 0
-
-
-def centre_config() -> bool:
-    out_put_array = [GPIO.input(pin) for pin in sensor_pins]
-    return out_put_array[1] == 1
-
-
-def out_config() -> bool:
-    out_put_array = [GPIO.input(pin) for pin in sensor_pins]
-    return out_put_array[0] == 0 and out_put_array[1] == 0
-
-
-def start() -> None:
-    GPIO.output(conveyer_ports[1], GPIO.LOW)
-    GPIO.output(conveyer_ports[0], GPIO.HIGH)
-    return
-
-
-def stop() -> None:
-    GPIO.output(conveyer_ports[0], GPIO.LOW)
-    GPIO.output(conveyer_ports[1], GPIO.HIGH)
-    return
 # -------------------------------------------------------------------
 
 
