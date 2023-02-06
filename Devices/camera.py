6f1836775556c192cbce93446c43f5fe25cbc60a
Shashank P <shashankp2832@gmail.com>
2023-02-06 18:24:50 +0530

    Commented image capture

--- Devices/camera.py
+++ Devices/camera.py
@@ -7,8 +7,6 @@ import imutils
 import cv2
 import sys
 from time import sleep
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 
 print("Initializing Camera ...")
 
@@ -27,18 +25,17 @@ def midpoint(ptA, ptB):
     return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
 
-def capture_image():
-    camera = PiCamera()
-    camera.resolution = camera.MAX_RESOLUTION
-    rawCapture = PiRGBArray(camera)
-    sleep(0.1)
-    camera.capture(rawCapture, format="rgb")
-    image = rawCapture.array
-    return image
+def capture_image(img_path):
+    # camera = PiCamera()
+    # camera.resolution = camera.MAX_RESOLUTION
+    # rawCapture = PiRGBArray(camera)
+    # sleep(0.1)
+    # camera.capture(rawCapture, format="bgr")
+    # image = rawCapture.array
+    return cv2.imread(img_path)
 
 
 def get_object_size(image, distance_bet_cam_obj, height_of_camera, pixel_per_metric):
-    cv2.imwrite("image.jpg", image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (7, 7), 0)
     edged = cv2.Canny(gray, 50, 100)
@@ -54,7 +51,6 @@ def get_object_size(image, distance_bet_cam_obj, height_of_camera, pixel_per_met
 
     max_dim_A = float('-inf')
     max_dim_B = float('-inf')
-
     for c in cnts:
         if cv2.contourArea(c) < 100:
             continue
@@ -65,6 +61,10 @@ def get_object_size(image, distance_bet_cam_obj, height_of_camera, pixel_per_met
         box = np.array(box, dtype="int")
 
         box = perspective.order_points(box)
+        cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+
+        for (x, y) in box:
+            cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
 
         (tl, tr, br, bl) = box
         (tltrX, tltrY) = midpoint(tl, tr)
@@ -72,6 +72,16 @@ def get_object_size(image, distance_bet_cam_obj, height_of_camera, pixel_per_met
 
         (tlblX, tlblY) = midpoint(tl, bl)
         (trbrX, trbrY) = midpoint(tr, br)
+
+        cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+        cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+        cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+        cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+
+        cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+                 (255, 0, 255), 2)
+        cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+                 (255, 0, 255), 2)
 
         dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
         dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
@@ -81,14 +91,54 @@ def get_object_size(image, distance_bet_cam_obj, height_of_camera, pixel_per_met
         dimA = (dA * d1) / (pixelsPerMetric * d0)
         dimB = (dB * d1) / (pixelsPerMetric * d0)
 
-        if discard_outlier(dimA, dimB):
-            print("Discarding outlier", dimA, dimB)
-            continue
-        print("Dimensions", dimA, dimB)
+        cv2.putText(orig, "{:.3f}cm".format(dimA),
+                    (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.65, (255, 255, 255), 2)
+        cv2.putText(orig, "{:.3f}cm".format(dimB),
+                    (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.65, (255, 255, 255), 2)
+
+        cv2.imshow("Image", orig)
+        cv2.waitKey(0)
+
         max_dim_A = max(max_dim_A, dimA)
         max_dim_B = max(max_dim_B, dimB)
 
     return {"length": max(max_dim_A, max_dim_B), "breadth": min(max_dim_A, max_dim_B)}
+    # for c in cnts:
+    #     if cv2.contourArea(c) < 100:
+    #         continue
+
+    #     orig = image.copy()
+    #     box = cv2.minAreaRect(c)
+    #     box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+    #     box = np.array(box, dtype="int")
+
+    #     box = perspective.order_points(box)
+
+    #     (tl, tr, br, bl) = box
+    #     (tltrX, tltrY) = midpoint(tl, tr)
+    #     (blbrX, blbrY) = midpoint(bl, br)
+
+    #     (tlblX, tlblY) = midpoint(tl, bl)
+    #     (trbrX, trbrY) = midpoint(tr, br)
+
+    #     dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+    #     dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+
+    #     pixelsPerMetric = round(pixel_per_metric, 4)
+
+    #     dimA = (dA * d1) / (pixelsPerMetric * d0)
+    #     dimB = (dB * d1) / (pixelsPerMetric * d0)
+
+    #     if discard_outlier(dimA, dimB):
+    #         print("Discarding outlier", dimA, dimB)
+    #         continue
+    #     print("Dimensions", dimA, dimB)
+    #     max_dim_A = max(max_dim_A, dimA)
+    #     max_dim_B = max(max_dim_B, dimB)
+
+    # return {"length": max(max_dim_A, max_dim_B), "breadth": min(max_dim_A, max_dim_B)}
 
 
 def get_length_width(image, height):
@@ -101,5 +151,5 @@ def get_length_width(image, height):
     return object_size["length"], object_size["breadth"]
 
 
-image = capture_image()
+image = capture_image("image6.jpg")
 print(get_length_width(image, 0))
