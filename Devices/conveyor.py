c50c9e5605386bd77d2987b64993b198b883840d
Shashank P <shashankp2832@gmail.com>
2023-02-11 01:35:20 +0530

    out config change

--- Devices/conveyor.py
+++ Devices/conveyor.py
@@ -32,7 +32,7 @@ def centre_config() -> bool:
 
 def out_config() -> bool:
     out_put_array = [GPIO.input(pin) for pin in sensor_pins]
-    return out_put_array == [0, 0, 0, 0, 1] or out_put_array == [0, 0, 0, 0, 0]
+    return out_put_array == [0, 0, 0, 0, 1]
 
 
 def start() -> None:
